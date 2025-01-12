cafcd70412ee1a67d5054771f796f67d22183e31
BarryByte <abhayraj.12667@gmail.com>
2025-01-12 14:09:34 +0530

    Add user registration, login, and logout functionality

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { login, logout, signup, updateProfile, checkAuth } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.put("/check",protectRoute,checkAuth) 
+// whenever we refresh our application we call checkAuth function
+export default router;
